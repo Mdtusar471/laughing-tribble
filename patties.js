5459159c16b0a6bcd4dade623005e4b37eee2824
Mdtusar471 <113382473+Mdtusar471@users.noreply.github.com>
2022-10-08 20:28:15 +0600

    Add files via upload

---
+++ patties.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("h2").click(function(){
+    alert("mdakbor471@yahoo.com")
+ 
+  });
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("p").toggle (1000)
+    })
+  });
+  
+});
+$("dol").hide(2000);
